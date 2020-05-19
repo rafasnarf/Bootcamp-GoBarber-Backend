a5e99125fd2d48326ffceb7c2de8572a1330d164
rafasnarf <snarfelcv@gmail.com>
2020-05-19 15:17:28 -0300

    Backend em desenvolvimento

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '7b6e3002d8e2a7f0c69a4d854587f2e1',
+    expiresIn: '1d',
+  },
+};
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
 import AppointmnetsRepository from '../repositories/AppointmentsRepository';
+import AppError from '../errors/AppError';
 
 interface Request {
   provider_id: string;
@@ -20,7 +21,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentInSameDate) {
-      throw Error('This appointment is already booked');
+      throw AppError('This appointment is already booked');
     }
 
     const appointment = appointmentsRepository.create({
